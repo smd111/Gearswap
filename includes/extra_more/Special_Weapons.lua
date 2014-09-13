ee3b4588c7f98a67b9bd4650e47ed32dbad05b46
smd111 <smd111@gmail.com>
2014-09-13 18:45:01 -0400

    fixed special weapon code

--- includes/extra_more/Special_Weapons.lua
+++ includes/extra_more/Special_Weapons.lua
@@ -3,145 +3,117 @@ if not specialweaponwscount then
 end
 function special_weapon()
 	if spell.type == "WeaponSkill" then
-		if player.equipment.main == "Molva Maul" then --club
+		if player.equipment.main == "Molva Maul" and spell.name ~= "Randgrith" then --club
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Randgrith" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Skogul Lance" then --polearm
+		elseif player.equipment.main == "Skogul Lance" and spell.name ~= "Geirskogul" then --polearm
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Geirskogul" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Cleofun Axe" then --axe
+		elseif player.equipment.main == "Cleofun Axe" and spell.name ~= "Onslaught" then --axe
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Onslaught" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Corbenic Sword" then --sword
+		elseif player.equipment.main == "Corbenic Sword" and spell.name ~= "Knights of Round" then --sword
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Knights of Round" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Murti Bow" then --archery
+		elseif player.equipment.main == "Murti Bow" and spell.name ~= "Namas Arrow" then --archery
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Namas Arrow" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Heofon Knuckles" then --hand to hand
+		elseif player.equipment.main == "Heofon Knuckles" and spell.name ~= "Final Heaven" then --hand to hand
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Final Heaven" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Clement Skean" then --dagger
+		elseif player.equipment.main == "Clement Skean" and spell.name ~= "Mercy Stroke" then --dagger
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
-				send_command('input /ws "Randgrith" <t>')
+				send_command('input /ws "Mercy Stroke" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Khloros Blade" then --great sword
+		elseif player.equipment.main == "Khloros Blade" and spell.name ~= "Scourge" then --great sword
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Scourge" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Barbarus Bhuj" then --great axe
+		elseif player.equipment.main == "Barbarus Bhuj" and spell.name ~= "Metatron Torment" then --great axe
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Metatron Torment" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Crisis Scythe" then --scythe
+		elseif player.equipment.main == "Crisis Scythe" and spell.name ~= "Catastrophe" then --scythe
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Catastrophe" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Sekirei" then --katana
+		elseif player.equipment.main == "Sekirei" and spell.name ~= "Blade: Metsu" then --katana
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Blade: Metsu" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Ame-no-ohabari" then --great katana
+		elseif player.equipment.main == "Ame-no-ohabari" and spell.name ~= "Tachi: Kaiten" then --great katana
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Tachi: Kaiten" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Chthonic Staff" then --staff
+		elseif player.equipment.main == "Chthonic Staff" and spell.name ~= "Gate of Tartarus" then --staff
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Gate of Tartarus" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
-		elseif player.equipment.main == "Exequy Gun" then --marksmanship
+		elseif player.equipment.main == "Exequy Gun" and spell.name ~= "Coronach" then --marksmanship
 			if specialweaponwscount < 13 then
 				specialweaponwscount = specialweaponwscount +1
-				file_write()
 			elseif specialweaponwscount == 13 then
 				cancel_spell()
 				send_command('input /ws "Coronach" <t>')
 				specialweaponwscount = 0
-				file_write()
 			end
 		end
 	end
